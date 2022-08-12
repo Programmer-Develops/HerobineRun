312122fdab1912529ce09b1f097096f739b28c06
Programmer Develops <74005248+Programmer-Develops@users.noreply.github.com>
2022-08-12 14:52:06 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1660295895,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
